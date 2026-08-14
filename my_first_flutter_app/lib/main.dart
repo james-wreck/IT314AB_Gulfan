746c052acd8845f0d6fc28a9c07e0ef1e16a3454
james-wreck <jameslaurence.gulfan@dbtc-cebu.edu.ph>
2026-08-14 13:27:25 +0800

    feat: submit capture the flag 3

--- my_first_flutter_app/lib/main.dart
+++ my_first_flutter_app/lib/main.dart
@@ -4,6 +4,44 @@ void main() {
   runApp(const MyApp());
 }
 
+List<Map<String, dynamic>> profiles = [
+  {
+    'image': 'assets/da.jfif',
+    'name': "James Laurence C. Gulfan",
+    'courseSection': "BSIT - 3",
+    'age': 19,
+    'hobby': "Basketball",
+  },
+  {
+    'image': 'assets/john.jfif',
+    'name': "John Deacon",
+    'courseSection': "BSIT - 3",
+    'age': 23,
+    'hobby': null,
+  },
+  {
+    'image': 'assets/brian.jfif',
+    'name': "Brian May",
+    'courseSection': "BSIT - 3",
+    'age': null,
+    'hobby': "Sleeping",
+  },
+  {
+    'image': 'assets/fred.jfif',
+    'name': "Fred Merc",
+    'courseSection': "BSIT - 3",
+    'age': 20,
+    'hobby': "Singing",
+  },
+  {
+    'image': 'assets/roger.jfif',
+    'name': "Roger Taylor",
+    'courseSection': null,
+    'age': 26,
+    'hobby': "Dancing",
+  },
+];
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -18,52 +56,66 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.teal,
         ),
         body: Center(
-          child: Card(
-            margin: const EdgeInsets.all(20),
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    "assets/download.jpg", // Change this if your filename is different
-                    width: 100,
-                    height: 100,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ListView(
+              children: [
+                ...profiles.map(
+                  (profile) => Card(
+                    margin: const EdgeInsets.all(20),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            profile['image'],
+                            width: 100,
+                            height: 100,
+                          ),
+                          const SizedBox(height: 15),
+                          Text(
+                            profile['name'] ?? "Name: Empty",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            profile['courseSection'] ?? "Course: Empty",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            "AGE: ${profile['age'] ?? "Empty"}",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            "Hobby : ${profile['hobby'] ?? "Empty"}",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                        ],
+                      ),
+                    ),
                   ),
-
-                  const SizedBox(height: 15),
-
-                  const Text(
-                    "James Laurence C. Gulfan",
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                  ),
-
-                  const SizedBox(height: 8),
-
-                  const Text("BSIT - 3", style: TextStyle(fontSize: 18)),
-
-                  const SizedBox(height: 8),
-
-                  const Divider(),
-
-                  const Text(
-                    "My First Flutter Application",
-                    style: TextStyle(fontSize: 18),
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  const Icon(
-                    Icons.sentiment_very_satisfied,
-                    color: Colors.amber,
-                    size: 40,
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  const Text("August 4, 2026", style: TextStyle(fontSize: 16)),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
