d46a3ff783a2c494485da854a00401df3a37d88b
james-wreck <jameslaurence.gulfan@dbtc-cebu.edu.ph>
2026-09-14 13:09:44 +0800

    docs: submit ctf 6 pdf

---
+++ exam_2/.dart_tool/flutter_build/a5c585000e8127b8e32e6682f2972290/install_code_assets.d
@@ -0,0 +1,1 @@
+ C:\\Users\\Student\\IT314AB_Gulfan\\exam_2\\.dart_tool\\flutter_build\\a5c585000e8127b8e32e6682f2972290\\native_assets.json: 
---
+++ exam_2/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Student\\flutter_windows_3.44.9-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Student\\IT314AB_Gulfan\\exam_2" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Student\\flutter_windows_3.44.9-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Student\\IT314AB_Gulfan\\exam_2"
+  "FLUTTER_ROOT=C:\\Users\\Student\\flutter_windows_3.44.9-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Student\\IT314AB_Gulfan\\exam_2\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Student\\IT314AB_Gulfan\\exam_2"
+  "FLUTTER_TARGET=C:\\Users\\Student\\IT314AB_Gulfan\\exam_2\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuOQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NmIxODJkMmM3NQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NWEyYTZhNDJjYw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4y"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Student\\IT314AB_Gulfan\\exam_2\\.dart_tool\\package_config.json"
+)
